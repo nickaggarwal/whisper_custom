97d7b487c3feabaa7959d9fa831e00a998b9deed
Inferless <code@inferless.com>
2023-05-12 09:49:48 +0000

    inferless commit

---
+++ 1/app.py
@@ -0,0 +1,17 @@
+import json
+import numpy as np
+import torch
+from transformers import pipeline
+
+
+class InferlessPythonModel:
+    def initialize(self):
+        self.generator = pipeline("automatic-speech-recognition", model="vasista22/whisper-hindi-large-v2",device=0)
+
+    def infer(self, audio_url):
+        pipeline_output = self.generator(audio_url)
+        generated_txt = pipeline_output["text"]
+        return generated_txt
+
+    def finalize(self):
+        self.pipe = None
