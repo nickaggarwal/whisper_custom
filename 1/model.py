6b6919f55bdfab5b0752712d41ca2e6faa19cdee
Nilesh Agarwal <nick.aggarwal@gmail.com>
2023-05-14 12:45:59 +0530

    Update model.py

--- 1/model.py
+++ 1/model.py
@@ -20,7 +20,7 @@ class TritonPythonModel:
             input = pb_utils.get_input_tensor_by_name(request, "language")
             input_language = input.as_numpy()[0].decode()
             input = pb_utils.get_input_tensor_by_name(request, "return_timestamps")
-            input_timestamps = input.as_numpy()[0].decode()
+            input_timestamps = input.as_numpy()[0]
             output = inferless_model.infer(input_string, input_language, input_task, input_timestamps)
             inference_response = pb_utils.InferenceResponse(
                 output_tensors=[
